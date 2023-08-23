eb2528cd0b99f50ce2aa1bc0248b9aee7946dde1
ateeq.abid1@hscic.gov.uk <ateeq.abid1@hscic.gov.uk>
2023-08-23 09:38:37 +0100

    CAAS-1394: 404 test update

--- tests/test_404_errors.py
+++ tests/test_404_errors.py
@@ -6,17 +6,18 @@ for more ideas on how to test the authorization of your API.
 import requests
 import pytest
 from lib import Assertions
+from lib import Generators
 from lib.constants import CORRELATION_IDS, METHODS
 
 
 @pytest.mark.functionaltest
 @pytest.mark.parametrize("correlation_id", CORRELATION_IDS)
 @pytest.mark.parametrize("method", METHODS)
-def test_404_not_found(nhsd_apim_proxy_url, nhsd_apim_auth_headers, correlation_id, method):
-    error_response = getattr(requests, method)(f"{nhsd_apim_proxy_url}", headers={
-        **nhsd_apim_auth_headers,
-        "X-Correlation-Id": correlation_id
-    })
+def test_404_not_found(nhsd_apim_proxy_url, correlation_id, method):
+    error_response = getattr(requests, method)(
+        f"{nhsd_apim_proxy_url}/invalid",
+        headers=Generators.generate_target_server_headers(correlation_id)
+    )
 
     Assertions.assert_error_with_optional_correlation_id(
         error_response,
