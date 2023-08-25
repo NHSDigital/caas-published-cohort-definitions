383c8095f6217c66795d6d9890d6d0af3a209e72
ateeq.abid1@hscic.gov.uk <ateeq.abid1@hscic.gov.uk>
2023-08-25 15:36:17 +0100

    CAAS-1394: run-tests update include api_key

--- tests/lib/generators.py
+++ tests/lib/generators.py
@@ -11,7 +11,7 @@ class Generators():
     def generate_target_server_headers(correlation_id):
         return {
             "Content-Type": "application/json",
-            "x-api-key": os.environ.get("API_KEY"),
+            "x-api-key": os.environ.get("INTEGRATION_API_KEY"),
             "X-Correlation-ID": correlation_id,
             "X-Request-ID": "b452ba10-6783-449d-b23e-da146ea27140",
         }
